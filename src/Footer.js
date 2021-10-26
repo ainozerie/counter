4ba7450d01b2ba01988fd7d270a8970b4ac1aee6
BC-macair014 <bc-macair014@BC-macair014s-MacBook-Air.local>
2021-10-26 12:08:51 +0300

    All works. Counter doesn't go below zero, changes background-color(className) (?even or odd)

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,13 @@
-function Footer() {
-    return (
-        <footer>
-            <p>Ainozerov Sergei. All copyrighrts reserved.</p>
-        </footer>
-    );
+import React, {Component} from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer>
+                <p>Ainozerov Sergei. All copyrighrts reserved.</p>
+            </footer>
+        );
+    }
 }
 
 export default Footer;
--- src/Main.js
+++ src/Main.js
@@ -1,20 +1,55 @@
-function Main() {
-    return (
-        <main>
-            <screen>
-                <h2>0</h2>
-            </screen>
-            <buttonsBar>
-                <button>Add five</button>
-                <button>Add one</button>
-                <button>Remove one</button>
-                <button>Remove five</button>
-            </buttonsBar>
-            <resetButton>
-                <button>Reset</button>
-            </resetButton>
-        </main>
-    );
+/* eslint-disable default-case */
+/* eslint-disable no-fallthrough */
+import React, {Component} from 'react';
+
+class Main extends Component {
+    state = {
+        counter: 2,
+        text: 'Hello world!'
+    }
+    addOne = () => {
+        this.setState({counter: this.state.counter + 1});
+    };
+    addFive = () => {
+        this.setState({counter: this.state.counter + 5});
+    };
+    removeOne = () => {
+        this.setState({counter: this.state.counter - 1});
+    };
+    removeFive = () => {
+        this.setState({counter: this.state.counter - 5});
+    };
+    resetCounter = () => {
+        this.setState({counter: 0});
+    };
+    
+    render() {
+        let evenOrOdd = '';
+        if (this.state.counter % 2 === 0) {
+            evenOrOdd = 'even';
+        } else {
+            evenOrOdd = 'odd';
+        }
+        if (this.state.counter < 0) {
+            this.setState({counter: 0});
+        }
+        return (
+            <main>
+                <screen className = {evenOrOdd} >
+                    <h2>{this.state.counter}</h2>
+                </screen>
+                <buttonsBar>
+                    <button onClick = {this.addFive} >Add five</button>
+                    <button onClick = {this.addOne}>Add one</button>
+                    <button onClick = {this.removeOne}>Remove one</button>
+                    <button onClick = {this.removeFive} >Remove five</button>
+                </buttonsBar>
+                <resetButton>
+                    <button onClick = {this.resetCounter}>Reset</button>
+                </resetButton>
+            </main>
+        );
+    }
 }
 
 export default Main;
